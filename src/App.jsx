1d6cfe10c9bfa77d58fe070d7149d084f566ee3f
danilopgon <dani.lopezgonzalez.94@gmail.com>
2023-06-07 12:59:53 +0200

    Finish Details and Title components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css';
-import { Navbar } from './components/Navbar';
+import "./App.css";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  
   return (
     <>
-    <Navbar/>
-      
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Title.jsx
@@ -0,0 +1,5 @@
+const Title = () => {
+  return <h2>Section title</h2>;
+};
+
+export default Title;
