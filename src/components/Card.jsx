dfbda29ca1d34a0b3c9c4f9e80b249bf394b5e38
michelcub <orestelopez96@gmail.com>
2023-06-08 19:12:15 +0100

    complete the card

---
+++ src/components/Card.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useState } from "react";
+import useAppContext from "../context/AppContext";
+export const Card = ({title,children, id}) => {
+
+    const [likeStatus, setLikeStatus] = useState(false)
+    const{store, actions} = useAppContext;
+
+    const switchStatus = () => {
+        likeStatus? actions.handleDeleteFavorites : actions.handleAddFavoritesList; 
+        setLikeStatus(!likeStatus);
+    }
+
+
+    return(
+        <div className="card col">
+            <img src="card-img-top" alt="img-default" />
+            <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                {children}
+            </div>
+            <div className="card-footer d-flex justify-content-around">
+                <button className="btn btn-outline-primary">Learn More!</button>
+                <button id={id} className="btn btn-outline-warning" onClick={switchStatus}><i id={id} className={` fa-regular fa-heart ${likeStatus&&'text-warning'}`} onClick={switchStatus}></i></button>
+            </div>
+        </div>
+    )
+}
