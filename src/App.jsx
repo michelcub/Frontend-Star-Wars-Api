0fdca02b8d74b518626070f99b3368a572233f87
michelcub <orestelopez96@gmail.com>
2023-06-08 19:33:14 +0100

    create routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import { Navbar } from './components/Navbar';
 
+import { Home } from './views/Home';
+import { Details } from './views/Details';
 function App() {
   
   return (
-    <>
-    <Navbar/>
-      
-    </>
+    <BrowserRouter basename='/'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/details/:id' element={<Details/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
